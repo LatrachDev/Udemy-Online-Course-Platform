9371272e99be6218d5eb00a261e57042e705eeb9
LatrachDev <m.latrach.youcode@gmail.com>
2025-01-16 23:26:48 +0100

    add cdn for tailwind instead of local

--- includes/errors/404.php
+++ includes/errors/404.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>404 - Page Not Found</title>
-    <script src="../assets/JavaScript/tailwind.js"></script>
+    <script src="https://cdn.tailwindcss.com"></script>
 </head>
 <body class="bg-gray-100 flex items-center justify-center h-screen">
     <div class="text-center">
--- pages/login.php
+++ pages/login.php
@@ -8,7 +8,7 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Login - Youdemy</title>
-  <script src="../assets/JavaScript/tailwind.js"></script>
+  <script src="https://cdn.tailwindcss.com"></script>
 </head>
 <body class="bg-gray-50 text-gray-800">
 
