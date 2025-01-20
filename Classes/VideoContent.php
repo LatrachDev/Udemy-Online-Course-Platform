576dd704f135ffae8a64ce638f98b0a8e3ac6946
LatrachDev <m.latrach.youcode@gmail.com>
2025-01-20 16:06:16 +0100

    UD-41 edit the video content class

--- Classes/VideoContent.php
+++ Classes/VideoContent.php
@@ -2,47 +2,26 @@
 require_once 'Content.php';
 class VideoContent extends Content
 {
-    private $vidUrl;
-    private $duration;
+    private $videoUrl;
 
-    public function __construct($db, $courseId, $vidUrl, $duration)
+    public function __construct($db, $courseId, $videoUrl)
     {
         parent::__construct($db, $courseId);
-        $this->vidUrl = $vidUrl;
-        $this->duration = $duration;
+        $this->videoUrl = $videoUrl;
     }
 
     public function save()
     {
-        $sql = "INSERT INTO contents (course_id, type) VALUES (?, 'video')";
-        $params = [
-            $this->courseId
-        ];
-
-        $this->db->query($sql, $params);
-
-        $contentId = $this->db->lastInsertId();
-
-        $sql = "INSERT INTO video_contents (content_id, video_url, duration) VALUES (?, ?, ?)";
-        $params = [
-            $contentId,
-            $this->vidUrl,
-            $this->duration
-        ];
-
-        $this->db->query($sql, $params);
-
-        return $this->db->lastInsertId();
+        $stmt = $this->db->prepare("INSERT INTO course_content (course_id, content_type, content_url) VALUES (?, 'video', ?)");
+        $stmt->execute([$this->courseId, $this->videoUrl]);
     }
 
     public function display($courseId)
     {
-        $sql = "SELECT title , DESCRIPTION , TYPE , video_url FROM courses
-        INNER JOIN contents ON courses.id = contents.course_id
-        INNER JOIN video_contents ON contents.id = video_contents.content_id
-        WHERE courses.id = ?";
+        $stmt = $this->db->prepare("SELECT content_url FROM course_content WHERE course_id = ? AND content_type = 'video'");
+        $stmt->execute([$courseId]);
+        $videoUrl = $stmt->fetchColumn();
 
-        $result = $this->db->fetch($sql, [$courseId]);
-        return $result;
+        return "<video controls><source src='$videoUrl' type='video/mp4'>Your browser does not support the video tag.</video>";
     }
 }
